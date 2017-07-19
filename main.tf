360902664f45bd35dcf8d505644db9c05ba419fd
Rohith <gambol99@gmail.com>
2017-07-20 00:26:20 +0100

    - the subnet data requires a vpc id

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ data "aws_vpc" "selected" {
 
 # Get a list of ELB subnets
 data "aws_subnet_ids" "selected" {
+  vpc_id = "${data.aws_vpc.selected.id}"
   tags   = "${merge(map("Role", var.elb_role_tag), map("Env", var.environment), var.subnet_tags)}"
 }
 
