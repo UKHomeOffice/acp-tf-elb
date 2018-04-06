f747ae40572fee0d580c2f71981cad806baa7f1c
Rohith <gambol99@gmail.com>
2018-04-06 14:29:07 +0100

    - updating the subnets filter to use a direct one to one

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "aws_vpc" "selected" {
 
 # Get a list of ELB subnets
 data "aws_subnet_ids" "selected" {
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "${data.aws_vpc.selected.id}"
   tags   = "${var.subnet_tags}"
 }
 
